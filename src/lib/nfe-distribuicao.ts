8a379b718da923c591d4887a42732d63a58ec7cc
Anderson Santos <anderfssantos@gmail.com>
2026-08-26 14:51:41 -0300

    Corrige cStat 215: assinatura não deve injetar atributo Id em distDFeInt
    O xml-crypto por padrão cria um atributo Id no elemento referenciado para
    apontar a Reference via URI="#id". O schema do distDFeInt não permite esse
    atributo extra, causando rejeição "Falha no esquema xml" pela Receita.
    Usando isEmptyUri para gerar Reference URI="" (autorreferência ao
    documento), como o serviço espera.

--- src/lib/nfe-distribuicao.ts
+++ src/lib/nfe-distribuicao.ts
@@ -66,6 +66,7 @@ function assinarXml(xml: string, certPem: string, chavePem: string): string {
   });
   sig.addReference({
     xpath: "//*[local-name(.)='distDFeInt']",
+    isEmptyUri: true,
     transforms: [
       "http://www.w3.org/2000/09/xmldsig#enveloped-signature",
       "http://www.w3.org/TR/2001/REC-xml-c14n-20010315",
